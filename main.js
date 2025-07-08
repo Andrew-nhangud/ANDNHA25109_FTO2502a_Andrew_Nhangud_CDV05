5e61069e74819e8863a5b92849ed9b9e2f4eecfa
Andrew Nhnagud <Andrewnhangud3785@gmail.com>
2025-07-08 15:43:28 +0200

    added imports in the main.js file

---
+++ main.js
@@ -0,0 +1,7 @@
+import { updateDate } from "./js/dateModule.js";
+import { initScrollAnimations } from "./js/scrollModule.js";
+
+window.onload = () => {
+  updateDate();
+  initScrollAnimations();
+};
